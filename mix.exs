cc08721eb9a2f8d865276cc3af6d68b7a8952cab
Marcus Lankenau <marcus.lankenau@2trde.com>
2017-08-26 11:37:02 +0200

    add package info and license

--- mix.exs
+++ mix.exs
@@ -7,8 +7,21 @@ defmodule Exml.Mixfile do
       version: "0.1.0",
       elixir: "~> 1.4",
       start_permanent: Mix.env == :prod,
+      name: "elixml",
+      description: "Easy to use xml lib. Allow to parse, compose and generate xml data",
+      package: [
+        files: ["lib", "config", "mix.exs", "LICENSE", "README*"],
+        maintainers: ["Marcus Lankenau"],
+        licenses: ["Apache 2.0"],
+        links: %{"GitHub" => "https://github.com/mlankenau/elixml"}
+      ],
+      source_url: "https://github.com/mlankenau/elixml"
       deps: deps()
     ]
+  end
+
+  defp description() do
+    
   end
 
   # Run "mix help compile.app" to learn about applications.
